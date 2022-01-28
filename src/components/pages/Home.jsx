64010cf68b0e290a431d8b0334a671afa52cd392
Sean M <mctastic2121@gmail.com>
2022-01-27 22:46:45 -0500

    added gif to homepage

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import "../../css/home.css"
+import FireGIF from "../../images/campfire-pixel.gif";
 
 const Home = () => {
     return (
         <div className="greeting">
             <h1>Greetings!<br/><br/>Stay Awhile!</h1>
+            <img src={FireGIF}/>
         </div>
     )
 }
